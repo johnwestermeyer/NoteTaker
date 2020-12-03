8cb9162386384a19f58ad6ce6fa161fa32c7b394
JohnWestermeyer <johnwestermeyer@gmail.com>
2020-12-03 07:49:26 -0800

    added additional index route

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
@@ -70,6 +74,8 @@ app.delete("/api/notes/:id", function(req, res){
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
+
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
